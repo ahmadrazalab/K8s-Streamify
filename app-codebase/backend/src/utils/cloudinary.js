c2f60ade9831c4a343a29b68fbeeba520e643fe2
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-01-07 01:05:43 +0530

    dotenv rollback + pod tag update

--- app-codebase/backend/src/utils/cloudinary.js
+++ app-codebase/backend/src/utils/cloudinary.js
@@ -6,10 +6,11 @@ import fs from "fs";
 // dotenv.config();
 
 
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dp8fisnhs",
+  api_key: "853372525533455",
+  api_secret: "GmDsZ-Uap-Z5bUpdtMVbzXvWUNc",
 });
 
 const uploadOnCloudinary = async (localFilePath, upload_options) => {
