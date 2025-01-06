85405c429e4a4dc358b0f1e7a4408993b97da934
ahmadrazalab <razahmadrsofficial@gmail.com>
2025-01-07 00:45:11 +0530

    removed the use of .env import from the code base to use the secrets

--- app-codebase/backend/src/index.js
+++ app-codebase/backend/src/index.js
@@ -1,10 +1,10 @@
 // require("dotenv").config({path : './env'})
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-dotenv.config({
-  path: "./.env",
-});
+// dotenv.config({
+//   path: "./.env",
+// });
 
 const port = process.env.PORT || 8000;
 
